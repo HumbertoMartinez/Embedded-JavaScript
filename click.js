22edd0bf9145c9b85a442524f89d6619ef349990
HumbertoMartinez <9hmartinez@stpatrick.org>
2017-04-05 05:48:16 -0500

    Making up work for my absence

--- click.js
+++ click.js
@@ -1,14 +1,28 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
   const doc = document;
+  let color ="#009900";
   let section = doc.querySelectorAll("#samecolor div");
   for(let h=0;h<section.length;h++){
-    section[h].style.backgroundColor=hex;
+    section[h].style.backgroundColor=color;
   }
+  section[0].stlyle.opacity= 0.3;
+  section[1].stlyle.opacity= 0.5;
+  section[2].stlyle.opacity= 0.7;
+  section[3].stlyle.opacity= 0.9;
 }
 //declare diffcolor() here
-function diffcolor(hex){
+function diffcolor(){
   const doc = document;
+  let color = "blue";
+  let complement = "yellow";
   let div = doc.querySelectorAll("#diffcolor div");
-  div.style.backgroundColor =
+  div[0].style.backgroundColor=color;
+  div[1].style.backgroundColor=complement;
+  div[2].style.backgroundColor=color;
+  div[3].style.backgroundColor=complement;
+  div[4].style.backgroundColor=color;
+  div[5].style.backgroundColor=complement;
+  div[6].style.backgroundColor=color;
+  div[7].style.backgroundColor=complement;
 }
